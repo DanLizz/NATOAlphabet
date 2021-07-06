00a7293f4c5b9475e5143f77cf203fd4b1a95e35
Dhanya Francis <63131743+DanLizz@users.noreply.github.com>
2021-07-05 23:23:40 -0400

    Catching exceptions

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
-#TODO 1. Create a dictionary in this format:
-
 word_data = pd.read_csv('nato_phonetic_alphabet.csv', index_col=0, squeeze=True).to_dict()
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-code = [word_data[letter] for letter in word]
-print(code)
+def generate_phonectic():
+    word = input("Enter a word: ").upper()
+    try:
+        code = [word_data[letter] for letter in word]
+    except KeyError:
+        print("Sorry, input alphabets only")
+        generate_phonectic()
+    else:
+        print(code)
+
+
+generate_phonectic()
